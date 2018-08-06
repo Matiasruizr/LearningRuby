042e7054bb002362cf9d38290e43fa48c9aca81c
matias <matiasruizr@hotmail.es>
2018-08-06 14:02:18 -0400

    Modelando televisor

---
+++ objetos_y_clases/televisor.rb
@@ -0,0 +1,56 @@
+class Televisor
+    # modelo tamaño volumen , encendido apagado cambiar canales
+    attr_accessor :modelo, :tamano
+    def initialize(params)
+        @modelo = params[:modelo]
+        @tamano = params[:tamano]
+        @volumen = 10
+        @estado = false
+        @canal = 1
+    end
+
+    def power
+        if @estado == true
+            @estado = false
+        else 
+            @estado = true
+        end
+    end
+
+    def presentacion
+        puts "Modelo: #{@modelo} Tamaño: #{@tamano} Volumen #{@volumen} Prendido: #{@estado} Canal actual #{@canal}"
+    end
+
+    def cambia_canal
+        @canal += 1
+        if @canal > 7
+            @canal = 1
+        end
+    end
+
+    def cambia_volumen
+        @volumen += 1
+        if @volumen > 50
+            puts 'El volumen esta al maximo'
+            @volumen = 49
+        end
+    end
+end
+
+lcd = Televisor.new(modelo:'LG', tamano: 32)
+
+lcd.presentacion
+lcd.power
+lcd.cambia_canal
+lcd.cambia_canal
+lcd.cambia_canal
+lcd.cambia_canal
+lcd.cambia_canal
+lcd.cambia_canal
+lcd.cambia_canal
+
+lcd.presentacion
+
+lcd.cambia_canal
+lcd.cambia_volumen
+lcd.presentacion
